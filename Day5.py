b2aff79235ac423baad05fb164f31c15a4713b29
Brighterbits <richardrazzell@icloud.com>
2024-12-05 15:49:43 +0000

    Day 5 COMPLETE! I don't even know how the insertion sort part works, I just copied the code from part 1

--- Day5.py
+++ Day5.py
@@ -1,3 +1,4 @@
+import math
 with open("input5.txt", "r") as f:
     switch = False
     Rules = []
@@ -14,6 +15,8 @@ with open("input5.txt", "r") as f:
     Rules = sorted(Rules)
     Rules = list(map(lambda x: x.replace("\n", "").split("|"), Rules))
     Lines = list(map(lambda x: x.replace("\n", "").split(","), Lines))
+    for i in range(len(Lines)):
+        Lines[i] = list(map(int, Lines[i]))
     RulesDict = {}
     for i in Rules:
         value = RulesDict.get(int(i[0]))
@@ -23,7 +26,37 @@ with open("input5.txt", "r") as f:
             value.append(int(i[1]))
             RulesDict[int(i[0])] = value
     print(RulesDict)
+    Correct = []
+    Incorrect = []
+    for Array in Lines: # For every single Array in the total number
+        Valid = True
+        for i in range(len(Array)): # Iterate over each part of the array
+            for j in RulesDict[Array[i]]: # Get the incorrect ones and then look backwards
+                if j in Array[0:i]:
+                    Valid = False
+                    break
+        if Valid:
+            Correct.append(Array[math.floor(len(Array)/2)])
+        else:
+            Incorrect.append(Array)
+    mIncorrect = []
+    for Array in Incorrect:
+        count = 0
+        while True:
+            try:
+                for j in RulesDict[Array[count]]:
+                    if j in Array[0:count]:
+                        buffer = Array[count]
+                        Array[count] = Array[count-1]
+                        Array[count-1] = buffer
+                        count = 0
+                        continue
+                count += 1
+            except:
+                break
+        mIncorrect.append(Array[math.floor(len(Array)/2)])
 
-    for Array in Lines:
-        for i in range(len(Array)):
-            
+    print(Correct)
+    print(sum(Correct))
+    print(Incorrect)
+    print(sum(mIncorrect))
