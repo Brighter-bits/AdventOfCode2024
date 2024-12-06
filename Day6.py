cdd1283022266399ccf90052b75645e820b046dc
Brighterbits <richardrazzell@icloud.com>
2024-12-06 23:22:22 +0000

    Matarme, que le mierda actual

--- Day6.py
+++ Day6.py
@@ -1,4 +1,3 @@
-from copy import deepcopy
 def p1():
     with open("input6.txt", "r") as f:
         Area = f.readlines()
@@ -12,21 +11,21 @@ def p1():
                 break
         loop = 0
         Direction = 0 # 0 is forwards, 1 is right, 2 is down, 3 is left
-        while 0 <= Xindex <= len(Area[0]) and 0 <= Yindex <= 129:
+        while 0 <= Xindex <= len(Area[0]) and 0 <= Yindex <= len(Area)-1:
             match Direction:
                 case 0:
                     if Area[Yindex-1][Xindex] == "#":
-                        Direction += 1
+                        Direction = (Direction+1)%4
                     else:
                         Yindex -= 1
                 case 1:
                     if Area[Yindex][Xindex+1] == '#':
-                        Direction += 1
+                        Direction = (Direction+1)%4
                     else:
                         Xindex += 1
                 case 2:
                     if Area[Yindex+1][Xindex] == '#':
-                        Direction += 1
+                        Direction = (Direction+1)%4
                     else:
                         Yindex += 1
                 case 3:
@@ -39,10 +38,12 @@ def p1():
         print(sum(total))
         print(loop)
 
+
 def p2():
-    with open("Input6.txt", "r") as f:
+    with open("input6.txt", "r") as f:
+        DirectionIndex = [[-1, 0], [0, 1], [1, 0], [0, -1]]
         Area = f.readlines()
-        Area = list(map(lambda x: list(x), Area))
+        Area = list(map(lambda x: list(x.strip()), Area))
         Xindex = -1
         Yindex = -1
         for i in range(130):
@@ -50,134 +51,149 @@ def p2():
                 Xindex = Area[i].index("^")
                 Yindex = i
                 break
-        loop = 0
+        # loop = 0
         FXindex = Xindex
         FYindex = Yindex
         Direction = 0 # 0 is forwards, 1 is right, 2 is down, 3 is left
-        Steps = []
+        # Steps = []
         for i in range(len(Area)):
             Area[i] = [e for e in Area[i] if e != "\n"]
 
-        while 0 <= Xindex <= len(Area[0])-1 and 0 <= Yindex <= 129:
-            Steps.append([Yindex, Xindex])
-            match Direction:
-                case 0:
+        # while Xindex in range(len(Area[i])) and Yindex in range(len(Area)):
+        #     Steps.append([Yindex, Xindex])
+        #     match Direction:
+        #         case 0:
+        #             try:
+        #                 if Area[Yindex-1][Xindex] == "#":
+        #                     Direction = (Direction+1)%4
+        #                 else:
+        #                     Yindex -= 1
+        #             except:
+        #                 break
+
+        #         case 1:
+
+        #             try:
+        #                 if Area[Yindex][Xindex+1] == '#':
+        #                     Direction = (Direction+1)%4
+        #                 else:
+        #                     Xindex += 1
+        #             except: break
+        #         case 2:
+
+        #             try:
+        #                 if Area[Yindex+1][Xindex] == '#':
+        #                     Direction = (Direction+1)%4
+        #                 else:
+        #                     Yindex += 1
+        #             except: break
+        #         case 3:
+        #             try:
+        #                 if Area[Yindex][Xindex-1] == '#':
+        #                     Direction = (Direction+1)%4
+        #                 else:
+        #                     Xindex -= 1
+        #             except: break
+        # print("\n")
+        # print(len(Steps))
+        # cache = Steps
+        # Steps = []
+        # for i in cache:
+        #     if i not in Steps:
+        #         Steps.append(i)
+        # print(len(Steps))
+        loop = 0
+        # for Coords in Steps:
+        for theY in range(len(Area)):
+            for theX in range(len(Area[0])):
+                Xindex = FXindex
+                Yindex = FYindex
+                Previous = set()
+                Continue = True
+                Direction = 0
+                LastNode = tuple()
+                LastLastNode = tuple()
+                # if (Coords[0] == FYindex and Coords[1] == FXindex):
+                #     Continue = False
+                # else:
+                #     Area[Coords[0]][Coords[1]] = "#"
+                if Area[theY][theX] == "#" or Area[theY][theX] == "^":
+                    continue
+                else:
+                    Area[theY][theX] = '#'
+
+                if loop == 6:
+                    pass
+                    # breakpoint()
+                while Xindex in range(len(Area[0])) and Yindex in range(len(Area)) and (Direction, Yindex, Xindex) not in Previous:
+                    match Direction:
+                        case 0:
+                            Previous.add((Direction, Yindex, Xindex))
+
+                            # try:
+                            #     if Area[Yindex-1][Xindex] == "#":
+                            #         Direction = (Direction+1)%4
+                            #         # LastLastNode = LastNode
+                            #         # LastNode = [Yindex-1, Xindex]
+                            # except Exception as e:
+                            #     print(e)
+                            #     Continue = False
+
+                        case 1:
+                            Previous.add((Direction, Yindex, Xindex))
+
+                            # try:
+                            #     if Area[Yindex][Xindex+1] == '#':
+                            #         Direction = (Direction+1)%4
+                            #         # LastLastNode = LastNode
+                            #         # LastNode = [Yindex-1, Xindex]
+                            # except Exception as e:
+                            #     print(e)
+                            #     Continue = False
+
+                        case 2:
+                            Previous.add((Direction, Yindex, Xindex))
+
+                            # try:
+                            #     if Area[Yindex+1][Xindex] == '#':
+                            #         Direction = (Direction+1)%4
+                            #         # LastLastNode = LastNode
+                            #         # LastNode = [Yindex-1, Xindex]
+                            # except Exception as e:
+                            #     print(e)
+                            #     Continue = False
+                        
+                        case 3:
+                            Previous.add((Direction, Yindex, Xindex))
+
+                            # try:
+                            #     if Area[Yindex][Xindex-1] == '#':
+                            #         Direction = 0
+                            #         # LastLastNode = LastNode
+                            #         # LastNode = [Yindex-1, Xindex]
+                            # except Exception as e:
+                            #     print(e)
+                            #     Continue = False
                     try:
-                        if Area[Yindex-1][Xindex] == "#":
-                            Direction += 1
+                        newY, newX = Yindex + DirectionIndex[Direction][0], Xindex + DirectionIndex[Direction][1]
+                        if newY in range(len(Area)) and newX in range(len(Area[0])) and Area[newY][newX] == "#":
+                            Direction = (Direction+1)%4
                         else:
-                            Yindex -= 1
-                    except:
-                        break
+                            Yindex = newY
+                            Xindex = newX
+                    except Exception as e:
+                        print(e)
+                        # Continue = False
+                    # LastLastNode = LastNode
+                    # LastNode = tuple([Yindex, Xindex])
 
-                case 1:
-
-                    try:
-                        if Area[Yindex][Xindex+1] == '#':
-                            Direction += 1
-                        else:
-                            Xindex += 1
-                    except: break
-                case 2:
-
-                    try:
-                        if Area[Yindex+1][Xindex] == '#':
-                            Direction += 1
-                        else:
-                            Yindex += 1
-                    except: break
-                case 3:
-                    try:
-                        if Area[Yindex][Xindex-1] == '#':
-                            Direction = 0
-                        else:
-                            Xindex -= 1
-                    except: break
-        print("\n")
-        print(len(Steps))
-        cache = Steps
-        Steps = []
-        for i in cache:
-            if i not in Steps:
-                Steps.append(i)
-        print(len(Steps))
-        for Coords in Steps:
-            Xindex = FXindex
-            Yindex = FYindex
-            Place = deepcopy(Area)
-            Continue = True
-            Direction = 0
-            if (Coords[0] == FYindex and Coords[1] == Xindex):
-                Continue = False
-            else:
-                Place[Coords[0]][Coords[1]] = "#"
-            if loop == 600:
-                # breakpoint()
-                pass
-            while Continue and (0 <= Xindex <= len(Area[0])-1 and 0 <= Yindex <= 129):
-                match Direction:
-                    case 0:
-                        if "U" in Place[Yindex][Xindex]:
-                            loop += 1
-                            Continue = False
-                        Place[Yindex][Xindex] += "U"
-
-                        try:
-                            if Place[Yindex-1][Xindex] == "#":
-                                Direction += 1
-                        except:
-                            Continue = False
-
-                    case 1:
-                        if "R" in Place[Yindex][Xindex]:
-                            loop += 1
-                            Continue = False
-                        Place[Yindex][Xindex] += "R"
-
-                        try:
-                            if Place[Yindex][Xindex+1] == '#':
-                                Direction += 1
-                        except:
-                            Continue = False
-
-                    case 2:
-                        if "D" in Place[Yindex][Xindex]:
-                            loop += 1
-                            Continue = False
-                        Place[Yindex][Xindex] += "D"
-
-                        try:
-                            if Place[Yindex+1][Xindex] == '#':
-                                Direction += 1
-                        except:
-                            Continue = False
-                    
-                    case 3:
-                        if "L" in Place[Yindex][Xindex]:
-                            loop += 1
-                            Continue = False
-
-                        Place[Yindex][Xindex] += "L"
-
-
-                        try:
-                            if Place[Yindex][Xindex-1] == '#':
-                                Direction = 0
-                        except:
-                            Continue = False
-
-                match Direction:
-                    case 0:
-                        Yindex -= 1
-                    case 1:
-                        Xindex += 1
-                    case 2:
-                        Yindex += 1
-                    case 3:
-                        Xindex -= 1
-
-            
-            # print("Complete", Coords, loop)
+                if (Direction, Yindex, Xindex) in Previous:
+                    loop += 1
+                    print(loop)
+                    Continue = False
+                # print("Complete", Coords, loop)
+                # Area[Coords[0]][Coords[1]] = "."
+                Area[theY][theX] = "."
         print(loop)
 
 p2()
--- ReadME.md
+++ ReadME.md
@@ -6,3 +6,6 @@ I lied, Day 6 is absolutely kicking my behind. The first part was simple enough,
 Part two is a lot harder. I've tried many methods but eventually optimised it down to simply putting an object on the squares the guard went to, 1966 was my answer. Wrong.
 More debugging, literal hours, and still nothing. Thus, having taken so long, I am going to ask the AI for a hint. A hint bear in mind, not an outright explanation. Thus I have failed
 Day 6, I hope it is a good learning point for me.
+
+It's been three hours, the AI was no help, I began looking at someone else's code, it looked very similar to mine, it seemed I had the right idea. No idea why I kept getting 1934.
+Never make me do Day 6 again. I was supposed to document each attempt and iteration, but I'm too tired for it.
