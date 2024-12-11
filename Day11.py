0df86524faf04f7f9199aa3e9850214c11f98d22
Brighter-bits <85905618+Brighter-bits@users.noreply.github.com>
2024-12-11 16:00:56 +0000

    No, I'm not happy

--- Day11.py
+++ Day11.py
@@ -3,16 +3,18 @@ def P1():
     with open("input11.txt", "r") as f:
         Stones = f.readline().split(" ")
         Zero = re.compile(r'^0+')
-        for i in range(25):
-            Cache = list(Stones)
+        for i in range(75):
+            # breakpoint()
+            Cache = []
             for number in range(len(Stones)):
+                # breakpoint()
                 if bool(re.search(Zero, Stones[number])):
                     Cache.append("1")
                 elif len(Stones[number]) % 2 == 0:
                     # breakpoint()
                     Part1, Part2 = Stones[number][:(int((len(Stones[number])/2)))], Stones[number][(int(len(Stones[number])/2)):]
-                    Cache.append(Part1)
-                    Cache.append(Part2)
+                    Cache.append(str(int(Part1)))
+                    Cache.append(str(int(Part2)))
                 else:
                     Cache.append(str(int(Stones[number]) * 2024)) 
             Stones = list(Cache)
